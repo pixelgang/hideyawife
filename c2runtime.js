bd67960bbe3b74d26c830ae7634d0c7cce96a69c
pixelgang <pixelgangus@gmail.com>
2017-08-26 21:32:04 -0400

    update windows upm

--- c2runtime.js
+++ c2runtime.js
@@ -22808,6 +22808,302 @@ cr.plugins_.admob = function(runtime)
 }());
 ;
 ;
+cr.plugins_.pubcenter = function(runtime)
+{
+	this.runtime = runtime;
+};
+(function ()
+{
+	var pluginProto = cr.plugins_.pubcenter.prototype;
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	var typeProto = pluginProto.Type.prototype;
+	typeProto.onCreate = function()
+	{
+	};
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	var instanceProto = pluginProto.Instance.prototype;
+	instanceProto.onCreate = function()
+	{
+		if (this.runtime.isDomFree)
+		{
+			cr.logexport("[Construct 2] Microsoft pubCenter plugin not supported on this platform");
+			return;
+		}
+		this.elem = document.getElementById("c2pubCenterAd");
+		if (!this.elem)
+		{
+			cr.logexport("[Construct 2] Microsoft pubCenter plugin not supported on this platform");
+			return;
+		}
+		this.appId = this.properties[0];
+		this.adUnitId = this.properties[1];
+		this.element_hidden = false;
+		this.visible = true;
+		this.x = -1000;
+		this.y = -1000;
+		this.width = 160;
+		this.height = 600;
+		this.lastLeft = 0;
+		this.lastTop = 0;
+		this.lastRight = 0;
+		this.lastBottom = 0;
+		this.lastWinWidth = 0;
+		this.lastWinHeight = 0;
+		this.bannerShowing = false;
+		this.interstitialAd = null;
+		this.interstitialBannerAd = null;
+		this.updatePosition(true);
+		this.runtime.tickMe(this);
+	};
+	instanceProto.onDestroy = function ()
+	{
+		if (this.runtime.isDomFree || !this.elem)
+			return;
+		jQuery(this.elem).remove();
+		this.elem = null;
+	};
+	instanceProto.tick = function ()
+	{
+		this.updatePosition();
+	};
+	var last_canvas_offset = null;
+	var last_checked_tick = -1;
+	instanceProto.updatePosition = function (first)
+	{
+		if (this.runtime.isDomFree || !this.elem)
+			return;
+		var left = this.x;
+		var top = this.y;
+		var right = this.x + this.width;
+		var bottom = this.y + this.height;
+		var curWinWidth = window.innerWidth;
+		var curWinHeight = window.innerHeight;
+		if (!this.visible)
+		{
+			if (!this.element_hidden)
+				jQuery(this.elem).hide();
+			this.element_hidden = true;
+			return;
+		}
+		if (!first && this.lastLeft === left && this.lastTop === top && this.lastRight === right && this.lastBottom === bottom && this.lastWinWidth === curWinWidth && this.lastWinHeight === curWinHeight)
+		{
+			if (this.element_hidden)
+			{
+				jQuery(this.elem).show();
+				this.element_hidden = false;
+			}
+			return;
+		}
+		this.lastLeft = left;
+		this.lastTop = top;
+		this.lastRight = right;
+		this.lastBottom = bottom;
+		this.lastWinWidth = curWinWidth;
+		this.lastWinHeight = curWinHeight;
+		if (this.element_hidden)
+		{
+			jQuery(this.elem).show();
+			this.element_hidden = false;
+		}
+		var offx = Math.round(left);
+		var offy = Math.round(top);
+		jQuery(this.elem).css("position", "absolute");
+		jQuery(this.elem).offset({left: offx, top: offy});
+		jQuery(this.elem).width(Math.round(right - left));
+		jQuery(this.elem).height(Math.round(bottom - top));
+	};
+	instanceProto.draw = function(ctx)
+	{
+	};
+	instanceProto.drawGL = function(glw)
+	{
+	};
+	function Cnds() {};
+	Cnds.prototype.IsBannerShowing = function ()
+	{
+		return this.bannerShowing;
+	};
+	Cnds.prototype.OnInterstitialReady = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.OnInterstitialError = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.OnInterstitialCompleted = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.OnInterstitialCancelled = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.OnBannerInterstitialReady = function () {
+	    return true;
+	};
+	Cnds.prototype.OnBannerInterstitialError = function () {
+	    return true;
+	};
+	Cnds.prototype.OnBannerInterstitialCompleted = function () {
+	    return true;
+	};
+	Cnds.prototype.OnBannerInterstitialCancelled = function () {
+	    return true;
+	};
+	pluginProto.cnds = new Cnds();
+	function Acts() {};
+	Acts.prototype.ShowBanner = function (pos, size)
+	{
+		if (this.runtime.isDomFree || !this.elem)
+			return;
+		switch (size) {
+		case 0:
+			this.width = 160;
+			this.height = 600;
+			break;
+		case 1:
+			this.width = 250;
+			this.height = 250;
+			break;
+		case 2:
+			this.width = 300;
+			this.height = 250;
+			break;
+		case 3:
+			this.width = 300;
+			this.height = 600;
+			break;
+		case 4:
+			this.width = 728;
+			this.height = 90;
+			break;
+		}
+		var winWidth = window.innerWidth;
+		var winHeight = window.innerHeight;
+		var x = 0;
+		var y = 0;
+		var myWidth = this.width;
+		var myHeight = this.height;
+		switch (pos) {
+		case 0:		// top-left
+			x = 0;
+			y = 0;
+			break;
+		case 1:		// top-center
+			x = winWidth / 2 - myWidth / 2;
+			y = 0;
+			break;
+		case 2:		// top-right
+			x = winWidth - myWidth;
+			y = 0;
+			break;
+		case 3:		// left
+			x = 0;
+			y = winHeight / 2 - myHeight / 2;
+			break;
+		case 4:		// center
+			x = winWidth / 2 - myWidth / 2;
+			y = winHeight / 2 - myHeight / 2;
+			break;
+		case 5:		// right
+			x = winWidth - myWidth;
+			y = winHeight / 2 - myHeight / 2;
+			break;
+		case 6:		// bottom left
+			x = 0;
+			y = winHeight - myHeight;
+			break;
+		case 7:		// bottom center
+			x = winWidth / 2 - myWidth / 2;
+			y = winHeight - myHeight;
+			break;
+		case 8:		// bottom right
+			x = winWidth - myWidth;
+			y = winHeight - myHeight;
+			break;
+		}
+		this.x = Math.round(x);
+		this.y = Math.round(y);
+		this.visible = true;
+		this.bannerShowing = true;
+	};
+	Acts.prototype.HideBanner = function ()
+	{
+		this.visible = false;
+		this.bannerShowing = false;
+	};
+	Acts.prototype.PrepareInterstitial = function (otherAdUnitId)
+	{
+		if (typeof MicrosoftNSJS === "undefined")
+			return;
+		this.interstitialAd = new MicrosoftNSJS["Advertising"]["InterstitialAd"]();
+		var self = this;
+		this.interstitialAd["onAdReady"] = function () {
+			self.runtime.trigger(cr.plugins_.pubcenter.prototype.cnds.OnInterstitialReady, self);
+		};
+		this.interstitialAd["onErrorOccurred"] = function () {
+			self.runtime.trigger(cr.plugins_.pubcenter.prototype.cnds.OnInterstitialError, self);
+		};
+		this.interstitialAd["onCompleted"] = function () {
+			self.runtime.trigger(cr.plugins_.pubcenter.prototype.cnds.OnInterstitialCompleted, self);
+		};
+		this.interstitialAd["onCancelled"] = function () {
+			self.runtime.trigger(cr.plugins_.pubcenter.prototype.cnds.OnInterstitialCancelled, self);
+		};
+		var adType = MicrosoftNSJS["Advertising"]["InterstitialAdType"]["video"];
+		this.interstitialAd["requestAd"](adType, this.appId, otherAdUnitId || this.adUnitId);
+	};
+	Acts.prototype.ShowInterstitial = function ()
+	{
+		if (!this.interstitialAd)
+			return;
+		if ((MicrosoftNSJS["Advertising"]["InterstitialAdState"]["ready"]) == (this.interstitialAd["state"]))
+		{
+			this.interstitialAd["show"]();
+		}
+	};
+	Acts.prototype.PrepareBannerInterstitial = function (otherAdUnitId) {
+	    if (typeof MicrosoftNSJS === "undefined")
+	        return;
+	    this.interstitialBannerAd = new MicrosoftNSJS["Advertising"]["InterstitialAd"]();
+	    var self = this;
+	    this.interstitialBannerAd["onAdReady"] = function () {
+	        self.runtime.trigger(cr.plugins_.pubcenter.prototype.cnds.OnBannerInterstitialReady, self);
+	    };
+	    this.interstitialBannerAd["onErrorOccurred"] = function () {
+	        self.runtime.trigger(cr.plugins_.pubcenter.prototype.cnds.OnBannerInterstitialError, self);
+	    };
+	    this.interstitialBannerAd["onCompleted"] = function () {
+	        self.runtime.trigger(cr.plugins_.pubcenter.prototype.cnds.OnBannerInterstitialCompleted, self);
+	    };
+	    this.interstitialBannerAd["onCancelled"] = function () {
+	        self.runtime.trigger(cr.plugins_.pubcenter.prototype.cnds.OnBannerInterstitialCancelled, self);
+	    };
+	    var adType = MicrosoftNSJS["Advertising"]["InterstitialAdType"]["display"];
+	    this.interstitialBannerAd["requestAd"](adType, this.appId, otherAdUnitId || this.adUnitId);
+	};
+	Acts.prototype.ShowBannerInterstitial = function () {
+	    if (!this.interstitialBannerAd)
+	        return;
+	    if ((MicrosoftNSJS["Advertising"]["InterstitialAdState"]["ready"]) == (this.interstitialBannerAd["state"])) {
+	        this.interstitialBannerAd["show"]();
+	    }
+	};
+	pluginProto.acts = new Acts();
+	function Exps() {};
+	pluginProto.exps = new Exps();
+}());
+;
+;
 cr.behaviors.Bullet = function(runtime)
 {
 	this.runtime = runtime;
@@ -24612,9 +24908,10 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Audio,
 	cr.plugins_.Keyboard,
 	cr.plugins_.Mouse,
+	cr.plugins_.Tilemap,
 	cr.plugins_.Touch,
 	cr.plugins_.Sprite,
-	cr.plugins_.Tilemap,
+	cr.plugins_.pubcenter,
 	cr.behaviors.EightDir,
 	cr.behaviors.scrollto,
 	cr.behaviors.bound,
@@ -24669,5 +24966,6 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.admob.prototype.acts.ShowInterstitial,
 	cr.plugins_.Audio.prototype.cnds.OnEnded,
 	cr.system_object.prototype.acts.RestartLayout,
-	cr.plugins_.admob.prototype.acts.ShowBanner
+	cr.plugins_.pubcenter.prototype.acts.PrepareBannerInterstitial,
+	cr.plugins_.pubcenter.prototype.acts.ShowBannerInterstitial
 ];};
